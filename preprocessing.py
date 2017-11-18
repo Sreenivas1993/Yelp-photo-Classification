3e3feafbc73857f7940a7d0c4860652dd3010bc1
Sreenivas1993 <sreenivenki@gmail.com>
2017-11-18 15:38:49 -0600

    commit

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd
 import sys
+import numpy as np
 if __name__=="__main__":
     #Taking input file as argument
     inputfile=sys.argv[1]
@@ -10,13 +11,23 @@ if __name__=="__main__":
     result=[]
     for item in json_data:
         my_dict={}
-        my_dict['photoid']=item.get('photo_id')+'.jpg'
+        photoid=item.get('photo_id')
+        my_dict['photoid']=photoid+'.jpg'
         my_dict['label']=item.get('label')
-        result.append(my_dict)
+        if(photoid[0]!='-'):
+            result.append(my_dict)
     df=pd.DataFrame(result)
     df=df.reindex(columns=['photoid','label'])
+    print('Statistics for each label before encoding')
+    value,freq=np.unique(df.ix[:,1],return_counts=True)
+    print(value)
+    print(freq)
     #Encoding categorical data
     from sklearn.preprocessing import LabelEncoder
     labelencoder=LabelEncoder()
     df.ix[:,1]=labelencoder.fit_transform(df.ix[:,1])
+    print('Statistics for each label after encoding')
+    value,freq=np.unique(df.ix[:,1],return_counts=True)
+    print(value)
+    print(freq)
     df.to_csv('out.csv',index=False)
