df103c4b15f971477630a5225d71fc3276574d20
Sreenivas1993 <sreenivenki@gmail.com>
2017-11-19 10:05:00 -0600

    commit

--- prediction.py
+++ prediction.py
@@ -10,20 +10,41 @@ from torch.autograd import Variable
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 import imagedataset as Image
 import sys
 import pandas as pd
+
 
 labelfile=['drink','food','inside','menu','outside']
 #Prediction of images
 def prediction(model,imageloader):
     pred=[]
+    #feeding input to network and extracting predictions
     for batch_idx,(data) in enumerate(imageloader):
         output=model(Variable(data))
         _,predicted=torch.max(output.data,1)
-        pred.append(labelfile[predicted])
+        print(predicted[0][0])
+        pred.append(labelfile[predicted[0][0]])
     return pred
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+        self.conv1=nn.Conv2d(3,5,5)
+        self.pool=nn.MaxPool2d(2)
+        self.conv2=nn.Conv2d(5,5,5)
+        self.fc1=nn.Linear(5*53*53,20)
+        self.fc2=nn.Linear(20,25)
+        self.fc3=nn.Linear(25,5)
+    def forward(self,x):
+        x=self.pool(F.relu(self.conv1(x)))
+        x=self.pool(F.relu(self.conv2(x)))
+        x=x.view(-1,5*53*53)
+        x=F.relu(self.fc1(x))
+        x=F.dropout(x,training=self.training)
+        x=F.relu(self.fc2(x))
+        x=F.dropout(x,training=self.training)
+        x=self.fc3(x)
+        return F.log_softmax(x)
 #Main function
 if __name__=="__main__":
     #Load modelfile,imagefolder,labelfile
